5c594b99db68abcce88456aad1b1b91b04ed5858
khangtruongvn <hkhag17@gmail.com>
2024-08-15 17:30:10 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import './App.css';
 import SyncViewSphere from './SyncViewReact360';
 
 const DUMMY_DATA = {
-  url: 'https://appcenter-missions-prod.s3.ap-southeast-1.amazonaws.com/blk-resorts-world-sentosa/rws_internal_inspection_11042024-festive_walk_-_escalator_-_no2_1DQYluEN5jgX2g/rws_II-festive_walk_escalator_no2/app_fi/360/rws_II-festive_walk_escalator_no2_0000.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAYPUGVDNQLX6SNJNQ%2F20240815%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Date=20240815T070435Z&X-Amz-Expires=28000&X-Amz-Signature=04578223e1715431d593687c4f7a884c336faab030f180d8cfdf5197c54a623d&X-Amz-SignedHeaders=host',
-  thumbnail:
-    's3://appcenter-missions-prod/blk-resorts-world-sentosa/rws_internal_inspection_11042024-festive_walk_-_escalator_-_no2_1DQYluEN5jgX2g/rws_II-festive_walk_escalator_no2/app_fi/thumbnails/rws_II-festive_walk_escalator_no2_0000.jpg',
+  url: '',
+  thumbnail: '',
   image_name: 'rws_II-festive_walk_escalator_no2_0000.jpg',
   camera_model: 'Non-drone',
   exif_image_width: 6144,
